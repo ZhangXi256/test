93ed3752c33e0f97e86928091a48d9313ef83f88
ZhangXi256 <zhangwuji156@sina.com>
2019-06-20 16:58:03 +0800

    read me

--- ForTest/ForTest/stdafx.cpp
+++ ForTest/ForTest/stdafx.cpp
@@ -6,4 +6,6 @@
 
 // TODO: 在 STDAFX.H 中引用任何所需的附加头文件，
 //而不是在此文件中引用
+
 merge conflict test, make a master modification again.
+merge conflict test, make a local modification and will stash it.
---
+++ This GIT ReadMe.txt
@@ -0,0 +1,5 @@
+zx      https://github.com/ZhangXi256/test (fetch)
+zx      https://github.com/ZhangXi256/test (push)
+
+git fetch zx
+git push zx master
